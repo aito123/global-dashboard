d861d1c34ab6c1f24161ee4a2f93dd8bfe65c6cd
aito123 <sotelog001@gmail.com>
2026-05-24 17:17:09 -0500

    update again

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
   },
   // If deploying to a GitHub Pages sub-path (e.g. username.github.io/repo-name)
   // uncomment and set the repo name:
-   basePath: "https://github.com/aito123//global-dashboard",
-   assetPrefix: "https://github.com/aito123//global-dashboard/",
+   basePath: "https://github.com/aito123/global-dashboard",
+   assetPrefix: "https://github.com/aito123/global-dashboard/",
 };
 
 module.exports = nextConfig;
